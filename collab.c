d436df1eabe0ab29db790ff3b9e8522ef9912a0b
peterPacho <piotrpacholarz@gmail.com>
2023-03-28 08:34:31 -0500

    added some stuff to my funciton

--- collab.c
+++ collab.c
@@ -27,6 +27,7 @@ void ppacho2_grohr2() {
     puts("Peter Pacholarz and Gavin Rohrer");
     puts("I learned that you need to make sure the polkit agent is running to clone over ssh");
     puts("I also learned that using git is easier than copy pasting to Google Drive");
+    puts( "Especially when trying to synchronize my work across the computers." );
 }
 
 void mausaf2_kperez39() {
