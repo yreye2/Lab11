96cc6250d69283fd4a1593db4b20c246bce50d6c
Samuel Haddad <129103266+shaddad3@users.noreply.github.com>
2023-03-28 15:04:47 +0000

    created new function with netID's shadd3 and alojko2

--- collab.c
+++ collab.c
@@ -23,6 +23,11 @@ void reed_ekidan2() {
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
 
+void shadd3_alojko2() {
+  printf("Sammy Haddad and Angela Lojko\n");
+  printf("From the readings, we learned that git and GitHub are really effective when working on large projects with many people.\n");
+}
+
 void ppacho2_grohr2() {
   puts("Peter Pacholarz and Gavin Rohrer");
   puts("I learned that you need to make sure the polkit agent is running to "
@@ -53,6 +58,7 @@ int main() {
       "This code has been created by the students of CS 211, Spring 2023!\n\n");
 
   reed_ekidan2();
+  shadd3_alojko2();
   mausaf2_kperez39();
   ppacho2_grohr2();
   dkhar3_nvaly2();
