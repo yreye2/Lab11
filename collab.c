8a59f736ea0d2256a5875f4b22d5c317f5fe4be9
Replit user <>
2023-03-28 13:56:09 +0000

    replit test

--- collab.c
+++ collab.c
@@ -1,52 +1,57 @@
 // Collaborative code file, Lab 11
-// 
+//
 // One person in your partner group should clone this repository.
-// Create a function that outputs your name, along with either one thing 
+// Create a function that outputs your name, along with either one thing
 //    that you learned about git or GitHub or something that gave you trouble
 //    that you would like others to learn from. The function name should be
 //    your NetID combined with your partner's NetID.
 // Feel free to add other things to your function as well, though be mindful
-// of the length of it, given how many students will be collaborating on this code!
-// 
+// of the length of it, given how many students will be collaborating on this
+// code!
+//
 // Read through the code that is already here before adding your own.
 // Add this code to the repository using git commands.
-// 
+//
 // An example is shown below.
-// 
+//
 // UIC CS 211, Spring 2023
 #include <stdio.h>
 
 void reed_ekidan2() {
-	// Adding comment here
+  // Adding comment here
   printf("Dale Reed and Ellen Kidane\n");
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
 
-
 void ppacho2_grohr2() {
-    puts("Peter Pacholarz and Gavin Rohrer");
-    puts("I learned that you need to make sure the polkit agent is running to clone over ssh\n");
-    puts("I also learned that using git is easier than copy pasting to Google Drive\n");
-    puts( "Especially when backing up / copying the projects between computers.\n" );
+  puts("Peter Pacholarz and Gavin Rohrer");
+  puts("I learned that you need to make sure the polkit agent is running to "
+       "clone over ssh\n");
+  puts("I also learned that using git is easier than copy pasting to Google "
+       "Drive\n");
+  puts(
+      "Especially when backing up / copying the projects between computers.\n");
 }
 
 void mausaf2_kperez39() {
-  printf("We learned that git push uplaods local changes to a remote repository\n");
+  printf("We learned that git push uplaods local changes to a remote "
+         "repository\n");
 }
 
 void dkhar3_nvaly2() {
   printf("Dev Kharvari and Nathan Valyaneth\n");
-  printf("We learned that GitHub is a collaborative cloud environment that makes collaborating on projects really easy and simple");
+  printf("We learned that GitHub is a collaborative cloud environment that "
+         "makes collaborating on projects really easy and simple");
 }
 
-int main()
-{
-    printf("This code has been created by the students of CS 211, Spring 2023!\n\n");
-    
-    reed_ekidan2();
-    mausaf2_kperez39();
-    ppacho2_grohr2();
-    dkhar3_nvaly2();
-    
-    return 0;
+int main() {
+  printf(
+      "This code has been created by the students of CS 211, Spring 2023!\n\n");
+
+  reed_ekidan2();
+  mausaf2_kperez39();
+  ppacho2_grohr2();
+  dkhar3_nvaly2();
+
+  return 0;
 }
